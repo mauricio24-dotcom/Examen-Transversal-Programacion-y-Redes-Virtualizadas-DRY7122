0841e0caf42cd6a4751cee2620c8293734557c9e
mauricio24-dotcom <mau.torresg@duocuc.cl>
2025-07-05 22:18:32 +0000

    Subiendo scripts python para examen transversal

---
+++ asn-bgp.py
@@ -0,0 +1,14 @@
+#AS BGP 16-bit y 32-bit Privado o Publico
+
+asn = int(input("Ingrese número de AS: "))
+
+if 1 <= asn <= 64511:
+    print(f"El AS {asn} es un AS público de 16 bits.")
+elif 64512 <= asn <= 65534:
+    print(f"El AS {asn} es un AS privado de 16 bits.")
+elif 65536 <= asn <= 4199999999:
+    print(f"El AS {asn} es un AS público de 32 bits.")
+elif 4200000000 <= asn <= 4294967294:
+    print(f"El AS {asn} es un AS privado de 32 bits.")
+else:
+    print(f"{asn} no es un número de AS válido de 16 o 32 bits.")
---
+++ integrantes.py
@@ -0,0 +1,12 @@
+#imprime nombres/apellidos integrantes de grupo
+def main():
+    integrantes = [
+        "Mauricio Torres",
+        "Vicente Vidal",
+    ]
+    print("Integrantes del grupo:")
+    for nombre in integrantes:
+        print(f"- {nombre}")
+
+if __name__ == "__main__":
+    main()
